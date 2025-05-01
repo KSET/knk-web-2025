d92e11e159f416a687572df4e201451c616e4ac9
Pavle Ergovic <pavleergovic@gmail.com>
2025-05-01 23:03:08 +0200

    finished header component for other pages

--- nuxt-app/components/Header.vue
+++ nuxt-app/components/Header.vue
@@ -1,10 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const visibleRight = ref(false);
+
+const toggleVisibleRight = (): void => {
+  visibleRight.value = !visibleRight.value;
+};
+
+</script>
+
 <template>
     <div class="header-wrapper">
       <div class="header-container">
         <img src="/assets/icons/burger.svg" alt="burger" @click="toggleVisibleRight" class="burger-icon"/>
         <div class="header-text-container">
-          <p class="header-text">kset</p>
-          <p class="header-text" style="text-align: center">na krku</p>
+          <p class="header-text">kset na krku</p>
+          <p class="header-text" style="text-align: center"></p>
           <!-- <p class="header-text" style="text-align: end">2025</p> -->
           <img src="/assets/icons/2025.svg" alt="2025" class="2025-icon" style="align-self: flex-end;" />
   
@@ -14,17 +25,47 @@
         <img src="/assets/icons/oblaci.svg" alt="oblaci" class="oblaci-icon" />
   
       </div>
-      <p class="header-date">14.-16.8.2025.</p>
     </div>
 
+
+<Drawer v-model:visible="visibleRight" hedaer=" " position="right">
+    <div class="drawer-wrapper">
+      <div class="drawer-container">
+        <div>
+          <NuxtLink to="/" class="hover:underline drawer-text" style="color: #844D99">Naslovnica</NuxtLink>
+        </div>
+        <div>
+          <NuxtLink to="/lineup" class="hover:underline drawer-text" style="color: #DD7D91">Izvođači</NuxtLink>
+        </div>
+        <div>
+          <NuxtLink to="/gallery" class="hover:underline drawer-text" style="color: #E55A8E">Galerija</NuxtLink>
+        </div>
+      </div>
+
+      <div class="drawer-footer">
+        <img src="/assets/icons/stolica.svg" alt="stolica" class="stolica-icon" />
+        <div class="zuta-podloga"></div>
+      </div>
+    </div>
+  </Drawer>
 </template>
 
+<style>
+
+.p-drawer {
+  background-color: #F7DEC0 !important;
+}
+
+.p-drawer-content {
+  padding: 0 !important;
+}
+</style>
 
 <style scoped>
 .header-wrapper {
   background-color: #76C6D2;
   width: 100%;
-  height: 15rem;
+  height: 6rem;
 }
 
 .header-container {
@@ -40,7 +81,8 @@
 .header-text-container {
   width: 65%;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  justify-content: space-evenly;
 }
 
 .header-text {
@@ -57,13 +99,14 @@
 
 .sunce-icon {
   position: absolute;
-  top: 10%;
-  left: 10%;
+  top: -100%;
+    width: 20%;
+    left: 10%;
 }
 
 .oblaci-icon {
   position: absolute;
-  top: 50%;
+  top: 15%;
   width: 100%;
 }
 
@@ -81,6 +124,66 @@
   right: 0%;
   top: 15%;
   cursor: pointer;
+  z-index: 20;
+  height: 60%;
 }
 
+.header-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.header-container {
+  max-width: 50rem;
+  width: 100%;
+}
+.drawer-wrapper {
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-rows: 60% 40%;
+}
+
+.drawer-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  height: 100%;
+}
+
+.drawer-text {
+  font-family: "Montserrat";
+  font-size: 1.5rem;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+
+.drawer-footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+
+.stolica-icon {
+  height: 80%;
+  z-index: 10;
+}
+
+.zuta-podloga {
+  background: #F3BB64;
+  position: absolute;
+  top: 70%;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
 </style>
