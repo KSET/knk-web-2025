81542c624600599c9a81a2adbed3eb051c198722
Pavle Ergović <pavleerg>
2025-08-02 20:41:56 +0200

    added google maps embed links to info page

--- nuxt-app/pages/info/index.vue
+++ nuxt-app/pages/info/index.vue
@@ -2,6 +2,7 @@
 import Header from '~/components/Header.vue'
 import Footer from '~/components/Footer.vue'
 import type { Translation } from '~/types/Translation'
+import { display } from '@primeuix/themes/aura/inplace'
 
 const query2 = groq`*[ _type == "translation"]`
 const { data: translationsRaw } = await useSanityQuery<Translation[]>(query2)
@@ -23,9 +24,34 @@ const translations = Object.fromEntries(
           class="info-text"
         />
       </div>
+
+      <div style="display: flex">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d365.7898541572364!2d14.5753015!3d45.0281636!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476373bfab3c4067%3A0xa899598a56cfc788!2sFran%20Krsto%20Frankopan%20Elementary%20School!5e1!3m2!1sen!2shr!4v1754159994657!5m2!1sen!2shr"
+          width="900  "
+          height="450"
+          style="border: 0; flex: 1"
+          allowfullscreen="true"
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+
       <h1 style="color: white">Informacije o kampu</h1>
       <div class="info-text">
         <BlockContent :blocks="translations?.infoKampText" class="info-text" />
+      </div>
+
+      <div style="display: flex">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d3230.7207613447326!2d14.557621187211181!3d45.026320259352104!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDXCsDAxJzMzLjQiTiAxNMKwMzMnMTguMyJF!5e1!3m2!1sen!2shr!4v1754159904806!5m2!1sen!2shr"
+          width="900  "
+          height="450"
+          style="border: 0; flex: 1"
+          allowfullscreen="true"
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
       </div>
     </div>
   </div>
